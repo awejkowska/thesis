b268c6d2071a68bbbaaf67971449b9e08ea584d6
Anna Wejkowska <awejkowska@op.pl>
2016-01-10 23:55:27 +0100

    edit urls to hide inzapp part

--- InzSite/urls.py
+++ InzSite/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^InzApp', include('InzApp.urls')),
+    url(r'^$', include('InzApp.urls')), #url(r'^InzApp', include('InzApp.urls')),
 )
